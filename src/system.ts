b78b669432aaef332da8d0e762865e0751ca2f6e
拼钢钢 <128269633+chatechat@users.noreply.github.com>
2023-03-19 16:42:03 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -13,5 +13,4 @@ export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.open
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
-
 export const resetContinuousDialogue = false
