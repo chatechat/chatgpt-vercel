eea5c87e838fc34c3241a0d7f95c1000f34a5b0e
拼钢钢 <128269633+chatechat@users.noreply.github.com>
2023-03-19 16:29:31 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,6 +10,7 @@ export const setting = {
 export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 - 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
 - 欢迎关注抖音拼钢钢，有问题可以进粉丝群讨论，本站为演示网站，KEY需要自己添加才可以使用。
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。`
 export type Setting = typeof setting
 
 export const resetContinuousDialogue = false
